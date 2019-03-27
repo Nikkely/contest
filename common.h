e0143345be251feb25f0182bd77c454792fb0227
Nikkely <umivudau153@gmail.com>
2019-03-28 00:07:21 +0900

    add UnionFindWithSize

--- common.h
+++ common.h
@@ -12,3 +12,20 @@
 typedef long long int lli;
 using namespace std;
 lli LARGE_NUM = 1000000007;
+
+inline void swap(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+inline int euclid(int a, int b) {
+    if (a > b) swap(a, b);
+    int r = a % b;
+    while (r > 0) {
+        a = b;
+        b = r;
+        r = a % b;
+    }
+    return b;
+}
