c8932be22b9832b53f94ff8e04905f1a798ee28b
Nikkely <umivudau153@gmail.com>
2019-03-27 23:58:59 +0900

    add common.h

---
+++ common.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef long long int lli;
+using namespace std;
+lli LARGE_NUM = 1000000007;
