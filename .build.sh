1fdc2ebd04fd4416ad98fdda0da813f5fc876af7
Nikkely <umivudau153@gmail.com>
2019-03-28 06:13:43 +0900

    add test script

---
+++ .build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cpplist=$(ls -a | grep .cpp)
+echo -e "target => \n$cpplist"
+
+targets=$(echo -e $cpplist|xargs)
+
+okflag=1
+for e in ${targets[@]}; do
+    echo "$e"
+    clang++ "$e"
+    ./a.out
+    if [ $? -ne 0 ]; then
+        echo "FAILUE"
+        okflag=0
+    else
+        echo "SUCCESS"
+    fi
+done
+
+if [ $okflag -eq 1 ]; then
+    echo "test clear"
+    exit 0
+else
+    echo "test failed"
+    exit 1
+fi
