f463d1f213c530d605b05d1e4b1144692a522fe7
Nikkely <umivudau153@gmail.com>
2019-05-11 20:45:41 +0900

    build with c++11

--- .build.sh
+++ .build.sh
@@ -8,7 +8,7 @@ targets=$(echo -e $cpplist|xargs)
 okflag=1
 for e in ${targets[@]}; do
     echo "$e"
-    clang++ "$e"
+    clang++ -std=c++11 "$e"
     ./a.out
     if [ $? -ne 0 ]; then
         echo "FAILUE"
