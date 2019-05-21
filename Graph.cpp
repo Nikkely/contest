c4ef4d7655f410106941b8fa8476e4214461092d
nihei_satoru <nihei_satoru@cocone.jp>
2019-05-21 13:53:06 +0900

    Add negative-loop check

--- Graph.cpp
+++ Graph.cpp
@@ -32,6 +32,27 @@ vector<int> bellman_ford(vector<Edge> edges, int V, int s) {
     return dist;
 }
 
+/**
+ * 負の閉路があるか調べる
+ * @order O(|V| * |E|)
+ * Bellman-Ford法を拡張して負の閉路の存在を調べる
+ **/
+bool has_negative_loop(vector<Edge> edges, int V) {
+    vector<int> dist(V);
+    int E = edges.size();
+    for (int i = 0; i < V; i++) dist[i] = 0;
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < E; j++) {
+            Edge e = edges[j];
+            if (dist[e.to] > dist[e.from] + e.cost) {
+                dist[e.to] = dist[e.from] + e.cost;
+                if (i == V - 1) return true;
+            }
+        }
+    }
+    return false;
+}
+
 int main() {
 
     vector<Edge> edges = {
@@ -44,5 +65,11 @@ int main() {
     assert(vector_check(bellman_ford(edges, 5, 0), {0, 4, 10, 15, 23}));
     assert(vector_check(bellman_ford(edges, 5, 2), {INF,INF, 0, INF, 13}));
     cout << "Bellman-Ford: PASS" << endl;
+    assert(!has_negative_loop(edges, 5));
+    edges.push_back(Edge{4, 0, -1});
+    assert(!has_negative_loop(edges, 5));
+    edges.push_back(Edge{4, 0, -30});
+    assert(has_negative_loop(edges, 5));
+    cout << "negative-loop check:PASS" << endl;
     return 0;
 }
