02c0aa1bfb494137f7af55ea7c7bf34dd593eb23
Nikkely <umivudau153@gmail.com>
2019-08-01 23:52:03 +0900

    マクロを追加した

--- common.h
+++ common.h
@@ -4,16 +4,44 @@
 #include <algorithm>
 #include <string>
 #include <queue>
+#include<iomanip>
 
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define rep(i,n) for(int i=0,i##_len=(n);i<i##_len;++i)
+#define reps(i,n) for(int i=1,i##_len=(n);i<=i##_len;++i)
+#define rrep(i,n) for(int i=((int)((n)-1));i>=0;--i)
+#define rreps(i,n) for(int i=((int)(n));i>0;--i)
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) ((int)(x).size())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define BIT(n) (1LL<<(n))
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#ifdef LOCAL
+    #define eprintf(...) fprintf(stderr, __VA_ARGS__)
+#else
+    #define eprintf(...) 4545
+#endif
+
 typedef long long int lli;
 using namespace std;
 typedef pair<int, int> ii;
+typedef priority_queue<int, vector<int>, greater<int> > heapq;
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
 // #define int long long int
+struct aaa{
+    aaa(){
+        cin.tie(0); ios::sync_with_stdio(0); cout<<fixed<<setprecision(20);
+    };
+}aaaaaaa;
+
+// signed main() {
+//     return 0;
+// }
 
 inline void swap(int &a, int &b) {
     int tmp = a;
@@ -21,11 +49,7 @@ inline void swap(int &a, int &b) {
     b = tmp;
 }
 
-// signed main() {
-//     return 0;
-// }
-
-// テスト用関数郡
+// テスト用関数群
 bool inline vector_check(vector<int> v1, vector<int> v2) {
     return v1.size() == v2.size() && equal(v1.cbegin(), v1.cend(), v2.cbegin());
 }
