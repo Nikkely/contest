8de1b7bc6c944aa43f67e8a27882b8e731af95fd
Nikkely <umivudau153@gmail.com>
2019-10-26 23:45:37 +0900

    refactor mod functions

--- Mod.cpp
+++ Mod.cpp
@@ -1,75 +1,99 @@
 #include "common.h"
 #include <cassert>
 
-const lli MOD = 1000000007LL;
-
-inline lli mod_add(lli x, lli y) {
-    return (x + y) % MOD;
-}
-
-inline lli mod_mul(lli x, lli y) {
-    return (x * y) % MOD;
-}
-
-inline lli mod_sub(lli x, lli y) {
-    return x - y >= 0 ? (x - y) % MOD : (x - y + MOD) % MOD;
-}
-
-inline lli mod_pow(lli x, lli y) {
-    lli ret = 1;
-    while (y > 0) {
-        if (y & 1) ret = ret * x % MOD;
-        x = x * x % MOD;
-        y = y >> 1;
+template<int MOD> struct Modnum {
+    lli val;
+    constexpr Modnum(lli v = 0) noexcept : val(v % MOD) {
+        if (val < 0) v += MOD;
     }
-    return ret;
-}
 
-// inline lli mod_inv(lli x, lli m) { // 拡張Euclid あんまよくわかってない
-//     lli b = m, u = 1, v = 0;
-//     while (b) {
-//         lli t = x / b;
-//         x -= t * b; swap(x, b);
-//         u -= t * v; swap(u, v);
-//     }
-//     u %= MOD;
-//     if (u < 0) u += m;
-//     return u;
-// }
+    constexpr Modnum operator - () const noexcept {
+        return val ? MOD - val : 0;
+    }
+    constexpr Modnum operator + (const Modnum& x) const noexcept {
+        return Modnum(*this) += x;
+    }
+    constexpr Modnum operator - (const Modnum& x) const noexcept {
+        return Modnum(*this) -= x;
+    }
+    constexpr Modnum operator * (const Modnum& x) const noexcept {
+        return Modnum(*this) *= x;
+    }
+    constexpr Modnum operator / (const Modnum& x) const noexcept {
+        return Modnum(*this) /= x;
+    }
 
+    constexpr Modnum& operator += (const Modnum& x) noexcept {
+        val += x.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Modnum& operator -= (const Modnum& x) noexcept {
+        val -= x.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Modnum& operator *= (const Modnum& x) noexcept {
+        val = val * x.val % MOD;
+        return *this;
+    }
+    constexpr Modnum& operator /= (const Modnum& x) noexcept {
+        val = val * modinv(x.val).val % MOD;
+        return *this;
+    }
 
-inline lli mod_inv(lli x) {
-    return mod_pow(x, MOD - 2);
-}
+    constexpr bool operator == (const Modnum& x) const noexcept {
+        return val == x.val;
+    }
+    constexpr bool operator != (const Modnum& x) const noexcept {
+        return val == x.val;
+    }
 
-inline lli mod_div(lli x, lli y) {
-    return x * mod_inv(y) % MOD;
-}
+    friend constexpr ostream& operator << (ostream &os, const Modnum<MOD>& x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr istream& operator >> (istream &is, const Modnum<MOD>& x) noexcept {
+        return is >> x.val;
+    }
+    static constexpr Modnum<MOD> modpow(const Modnum &x, lli n) noexcept {
+        if (n == 0) return 1;
+        auto t = modpow(x, n / 2);
+        t *= t;
+        if (n & 1) t *= x;
+        return t;
+    }
+    static constexpr Modnum<MOD> modinv(const Modnum &x) noexcept {
+        return modpow(x, MOD - 2);
+    }
+};
+
+using mi = Modnum<1000000007>;
 
 #define MAX_FAC 10000
-lli fac[MAX_FAC], finv[MAX_FAC];
+mi fac[MAX_FAC], finv[MAX_FAC];
 void init_fac() {
     fac[0] = fac[1] = 1;
     finv[0] = finv[1] = 1;
     for (int i = 2; i < MAX_FAC; i++) {
-        fac[i] = fac[i - 1] * i % MOD;
-        finv[i] = finv[i - 1] * mod_inv(i) % MOD;
+        fac[i] = fac[i - 1] * i;
+        finv[i] = finv[i - 1] * mi::modinv(i);
     }
 }
-inline lli com(int n, int k) {
+inline mi com(int n, int k) {
     if (n < k || n < 0 || k < 0) return 0;
-    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+    return fac[n] * (finv[k] * finv[n - k]);
 }
 
 int main() {
-    assert(mod_add(1000000007LL, 1LL) == 1LL);
-    assert(mod_mul(1000000007LL, 1LL) == 0LL);
-    assert(mod_sub(3LL, 1000000007LL) == 3LL);
-    assert(mod_pow(4LL, 3LL) == 64LL);
-    assert(mod_div(12LL, 3LL) == 4LL);
+    assert(mi(1000000007LL) + mi(1LL) == mi(1LL));
+    assert(mi(1000000007LL) * mi(1LL) == mi(0LL));
+    assert(mi(3LL) - mi(1000000007LL) == mi(3LL));
+    assert(mi::modpow(mi(4LL), 3LL) == mi(64LL));
+    assert(mi(12LL) / mi(3LL) == mi(4LL));
+    cout << "ModIntClass OK" << endl;
 
     init_fac();
-    assert(com(5LL, 3LL) == 10LL);
-    cout << "OK" << endl;
+    assert(com(5LL, 3LL) == mi(10LL));
+    cout << "Binomial coeffcients OK" << endl;
     return 0;
 }
