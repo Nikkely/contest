4afba949c218f38cb962e7846c73cbd4f3bd95cb
nikkely <umivudau153@gmail.com>
2021-08-09 18:34:24 +0900

    chore: for build Comb.cpp

--- test/run_test.sh
+++ test/run_test.sh
@@ -6,10 +6,13 @@ CPPLIST=$(cd $SRC_DIR/ && ls -a | grep .cpp)
 echo -e "target => \n$CPPLIST"
 TARGETS=$(echo -e $CPPLIST|xargs)
 
+REPO_URL='https://github.com/atcoder/ac-library.git'
+git clone REPO_URL
+
 OKFLAG=1
 for e in ${TARGETS[@]}; do
     echo -e "\n***${e}***"
-    g++ -g $SRC_DIR/$e -o a.out -std=gnu++17 -Wall -Wextra -Wshadow -Wconversion -ggdb
+    g++ -g $SRC_DIR/$e -o a.out -std=gnu++17 -Wall -Wextra -Wshadow -Wconversion -ggdb -I .
     ./a.out
     if [ $? -ne 0 ]; then
         echo "!!!FAILUE!!!"
